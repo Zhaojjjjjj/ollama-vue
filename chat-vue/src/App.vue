173e036765d47ed48fe2c3dc509361ac7b7a037a
zhj <zhj@10soo.com>
2024-06-21 16:14:42 +0800

    u

---
+++ chat-vue/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import ollama from 'ollama'
+
+let res = ref()
+
+onMounted(async () => {
+  await ollama
+    .chat({
+      model: 'llama3:8b',
+      messages: [{ role: 'user', content: 'Why is the sky blue?' }],
+    })
+    .then((response) => {
+      console.log(response)
+      res.value = response.message.content
+    })
+})
+</script>
+
+<template>
+  <div>
+    <p v-html="res"></p>
+  </div>
+</template>
+
+<style scoped></style>
